e6e635c5b7781061594457f5a00090b4ab5bab12
alberthsies <alberthsies@gmail.com>
2019-04-13 23:05:50 +0800

    Force push

--- tamsuiRiver/backend/models.py
+++ tamsuiRiver/backend/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Request(models.Model):
+    company = models.TextField(default="HKUST")
+    item = models.TextField(default="egg")
+    price = models.IntegerField(default=2)
+    quantity = models.IntegerField(default=1000)
+    farmer = models.TextField(default="Albert-Emily-")
+
+    class Meta:
+        db_table = "music"
--- tamsuiRiver/backend/views.py
+++ tamsuiRiver/backend/views.py
@@ -1,7 +1,8 @@
-#from django.shortcuts import render
+from django.shortcuts import render
 
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the backend index.")
+    return render(request, 'index.html')
+    #return HttpResponse("Hello, world. You're at the backend index.")
