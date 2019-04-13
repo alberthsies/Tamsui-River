db99d85b88e2bf25a7390f2278660489f33aae41
LiaoYihan <yh.emily90112@gmail.com>
2019-04-14 00:31:21 +0800

    Create table

--- tamsuiRiver/backend/models.py
+++ tamsuiRiver/backend/models.py
@@ -1,3 +1,5 @@
+
+
 from django.db import models
 
 # Create your models here.
@@ -10,4 +12,25 @@ class Request(models.Model):
     farmer = models.TextField(default="Albert-Emily-")
 
     class Meta:
-        db_table = "music"
+        db_table = "request"
+
+
+
+class Company(models.Model):
+    name = models.TextField(default="HKUST")
+    email = models.TextField(default="hkust@connect.ust.hk")
+
+    class Meta:
+        db_table = "company"
+
+class Farmer(models.Model):
+    name = models.TextField(default="Albert")
+    email = models.TextField(default="albert@connect.us.hk")
+    password = models.IntegerField(default="123456")
+    product = models.TextField(default="egg")
+    origin = models.TextField(default="Taipei City")
+    certificate = models.TextField(default="SGS")
+    description = models.TextField(default="We have the best eggs")
+
+    class Meta:
+        db_table = "farmer"
