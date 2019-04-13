ee1ef1a9e9373327125c6b38308f2321beed9454
alberthsies <alberthsies@gmail.com>
2019-04-13 21:17:32 +0800

    Update backend

--- tamsuiRiver/tamsuiRiver/settings.py
+++ tamsuiRiver/tamsuiRiver/settings.py
@@ -74,14 +74,14 @@ WSGI_APPLICATION = 'tamsuiRiver.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+#DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 # Password validation
